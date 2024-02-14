a8cce9e43d3329a09ca40149f803fe7c7d8689d4
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-14 16:18:26 -0600

    problem 5

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -3,8 +3,8 @@ Complete the python code to find the solution to the system of linear equations 
 The equations are of the form ax + by = c and dx + ey = f. The solution should be printed in the form x = # and y = #. 
 If there is no solution, print "There is no solution".
 
-Name:
-Lab Time:
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
 """
 
 def brute_eq():
@@ -19,6 +19,14 @@ def brute_eq():
     f = int(input())
 
     # YOUR CODE HERE
+    for x in range(-10, 11):
+        for y in range(-10, 11):
+            if ((a * x) + (b * y) == c) and ((d * x) + (e * y) == f):
+                print("x =", int(x), ", y =", int(y))
+                return
+
+    # If no solution is found
+    print("There is no solution")
     
 if __name__ == "__main__":
     brute_eq()
