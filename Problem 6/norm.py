170dae632f546aa7f39e48523f8b99823c155e6b
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-14 16:51:33 -0600

    problem 6

--- Problem 6/norm.py
+++ Problem 6/norm.py
@@ -1,12 +1,24 @@
 """
 Complete the following python code to take in a list of values from the user and then normalize them
 
-Name:
-Lab Time:
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
 """
 
 def norm():
     # Write your code here
+    num_values = int(input())
+    values = []
+
+    for _ in range(num_values):
+        value = float(input())
+        values.append(value)
+
+    max_value = max(values)
+
+    for value in values:
+        normalized_value = value / max_value
+        print(f'{normalized_value:.2f}')
 
 if __name__ == "__main__":
     norm()
