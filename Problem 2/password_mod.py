2f15083e3736965c14ebbcfd663230de5b4b2cca
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-14 14:39:57 -0600

    problem 2

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,14 +1,22 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
 """
 
 def password_mod():
     word = input()
     password = ''
     # Type your code here.
+    word = word.replace('i', '1')
+    word = word.replace('a', '@')
+    word = word.replace('m', 'M')
+    word = word.replace('B', '8')
+    word = word.replace('s', '$')
+
+    password = word + '!'
+    print(password)
 
 if __name__ == "__main__":
     password_mod()
