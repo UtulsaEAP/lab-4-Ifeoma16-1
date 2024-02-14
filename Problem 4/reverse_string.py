8e1f451cadd3ca32a23ccc4d453338792a8fa3b8
Ifeoma16 <ifeomalockwood@gmail.com>
2024-02-14 15:04:16 -0600

    problem 4

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -1,12 +1,19 @@
 """
 Complete the following python code to reverse the string entered by the user.
 
-Name: 
-Lab Time:
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
 """
 
 def reverse_string():
     # YOUR CODE HERE
+    while True:
+        user_string = str(input("Enter string of text (Done/done/d for exit): "))
+        if user_string == 'Done' or user_string == 'done' or user_string == 'd':
+            return 1
+        else:
+            reversed_string = user_string[::-1]
+            print(reversed_string)
     
 
 if __name__ == "__main__":
